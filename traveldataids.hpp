3f3da7ba47480038e21aab19092c7527de44dccd
sonjazorenc <50211142+sonjazorenc@users.noreply.github.com>
2019-05-22 09:37:12 +0200

    Create traveldataids.hpp

---
+++ traveldataids.hpp
@@ -0,0 +1,23 @@
+//Arbeitsauftrag ein Service-Offer für Fahrdaten zu erstellen, was über SOMEIP geschieht
+//15052019
+
+
+#ifndef VSOMEIP_EXAMPLES_SAMPLE_IDS_HPP
+#define VSOMEIP_EXAMPLES_SAMPLE_IDS_HPP
+
+#define SERVICE_ID       0x0001
+#define INSTANCE_ID      0x0010
+#define METHOD_ID        0x1000
+
+#define EVENT_ID         0x0100
+#define GET_METHOD_ID    0x3000
+#define SET_METHOD_ID    0x2000
+
+#define EVENTGROUP_ID    0x0200
+
+#define ANOTHER_SERVICE_ID 0x0002
+#define ANOTHER_INSTANCE_ID 0x0020
+#define ANOTHER_METHOD_ID  0x4000
+
+#endif // VSOMEIP_EXAMPLES_SAMPLE_IDS_HPP
+
